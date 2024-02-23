931c8f3faa3cba71e69a0d07710a58f894011457
Aluque <32274359+aluque1@users.noreply.github.com>
2024-02-23 14:14:46 +0100

    funciona p2 esta vez si que si

--- projects/lab2/lab2.runs/synth_1/lab2.tcl
+++ projects/lab2/lab2.runs/synth_1/lab2.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "/home/jorbis/Documentos/GitHub/DAS/projects/lab2/lab2.runs/synth_1/lab2.tcl"
+  variable script "/home/alv/dt/uni/DAS/projects/lab2/lab2.runs/synth_1/lab2.tcl"
   variable category "vivado_synth"
 }
 
@@ -70,29 +70,29 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param chipscope.maxJobs 4
+set_param xicom.use_bs_reader 1
+set_param chipscope.maxJobs 2
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a35tcpg236-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir /home/jorbis/Documentos/GitHub/DAS/projects/lab2/lab2.cache/wt [current_project]
-set_property parent.project_path /home/jorbis/Documentos/GitHub/DAS/projects/lab2/lab2.xpr [current_project]
+set_property webtalk.parent_dir /home/alv/dt/uni/DAS/projects/lab2/lab2.cache/wt [current_project]
+set_property parent.project_path /home/alv/dt/uni/DAS/projects/lab2/lab2.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property board_part digilentinc.com:basys3:part0:1.2 [current_project]
-set_property ip_output_repo /home/jorbis/Documentos/GitHub/DAS/projects/lab2/lab2.cache/ip [current_project]
+set_property ip_output_repo /home/alv/dt/uni/DAS/projects/lab2/lab2.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
 read_vhdl -library xil_defaultlib {
-  /home/jorbis/Documentos/GitHub/DAS/sources/common/common.vhd
-  /home/jorbis/Documentos/GitHub/DAS/sources/common/debouncer.vhd
-  /home/jorbis/Documentos/GitHub/DAS/sources/common/edgeDetector.vhd
-  /home/jorbis/Documentos/GitHub/DAS/sources/lab2/modCounter.vhd
-  /home/jorbis/Documentos/GitHub/DAS/sources/common/synchronizer.vhd
-  /home/jorbis/Documentos/GitHub/DAS/sources/lab2/lab2.vhd
+  /home/alv/dt/uni/DAS/sources/common/common.vhd
+  /home/alv/dt/uni/DAS/sources/common/debouncer.vhd
+  /home/alv/dt/uni/DAS/sources/common/edgeDetector.vhd
+  /home/alv/dt/uni/DAS/sources/lab2/modCounter.vhd
+  /home/alv/dt/uni/DAS/sources/common/synchronizer.vhd
+  /home/alv/dt/uni/DAS/sources/lab2/lab2.vhd
 }
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
@@ -103,10 +103,12 @@ OPTRACE "Adding files" END { }
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc /home/jorbis/Documentos/GitHub/DAS/sources/lab2/lab2.xdc
-set_property used_in_implementation false [get_files /home/jorbis/Documentos/GitHub/DAS/sources/lab2/lab2.xdc]
+read_xdc /home/alv/dt/uni/DAS/sources/lab2/lab2.xdc
+set_property used_in_implementation false [get_files /home/alv/dt/uni/DAS/sources/lab2/lab2.xdc]
 
 set_param ips.enableIPCacheLiteLoad 1
+
+read_checkpoint -auto_incremental -incremental /home/alv/dt/uni/DAS/projects/lab2/lab2.srcs/utils_1/imports/synth_1/lab2.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
