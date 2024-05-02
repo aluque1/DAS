2e4aa2b0bb9b62c80fa242edf0c1f8f0bde012d5
Jorbis21 <javierorbis@protonmail.com>
2024-05-02 02:02:10 +0200

    vgaText hecha, revisalo pls

--- sources/common/vgaTextInterface.vhd
+++ sources/common/vgaTextInterface.vhd
@@ -4,14 +4,14 @@
 --    vgaTextInterface.vhd  12/09/2023
 --
 --    (c) J.M. Mendias
---    Diseño Automático de Sistemas
---    Facultad de Informática. Universidad Complutense de Madrid
+--    Diseï¿½o Automï¿½tico de Sistemas
+--    Facultad de Informï¿½tica. Universidad Complutense de Madrid
 --
---  Propósito:
---    Genera las señales de color y sincronismo de un interfaz texto
---    VGA con resolución de 80x30 caracteres de 8x16 pixeles.
+--  Propï¿½sito:
+--    Genera las seï¿½ales de color y sincronismo de un interfaz texto
+--    VGA con resoluciï¿½n de 80x30 caracteres de 8x16 pixeles.
 --
---  Notas de diseño:
+--  Notas de diseï¿½o:
 --    - Para frecuencias a partir de 50 Mhz en multiplos de 25 MHz
 --    - Incluye una memoria de refresco para almacenar los caracteres
 --      a visualizar y una memoria de mapas de bits de cada caracter 
@@ -82,8 +82,12 @@ architecture syn of vgaTextInterface is
   signal romAddr     : std_logic_vector (11 downto 0);
   signal bitMapLine  : std_logic_vector (7 downto 0);
   signal bitMapPixel : std_logic;
+  
+  signal xy : std_logic_vector (11 downto 0);
+  signal clearxy : std_logic_vector (11 downto 0);
+  signal colrow : std_logic_vector (11 downto 0);
 
-  type   romType is array (0 to 2**12-1) of std_logic_vector (7 downto 0);  -- OJO: los pixeles están ubicados de izq. a der. y da igual que se cambie el range
+  type   romType is array (0 to 2**12-1) of std_logic_vector (7 downto 0);  -- OJO: los pixeles estï¿½n ubicados de izq. a der. y da igual que se cambie el range
   signal rom : romType := (
     X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00",    -- null
     X"00", X"7e", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"7e", X"00",    -- empty
@@ -124,7 +128,7 @@ architecture syn of vgaTextInterface is
     X"00", X"18", X"18", X"7c", X"c6", X"c2", X"c0", X"7c", X"06", X"86", X"c6", X"7c", X"18", X"18", X"00", X"00",    -- $
     X"00", X"00", X"00", X"00", X"00", X"c3", X"c6", X"0c", X"18", X"30", X"63", X"c3", X"00", X"00", X"00", X"00",    -- %
     X"00", X"00", X"00", X"38", X"6c", X"6c", X"38", X"76", X"dc", X"cc", X"cc", X"76", X"00", X"00", X"00", X"00",    -- &
-    X"00", X"00", X"30", X"30", X"30", X"60", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00",    -- ´
+    X"00", X"00", X"30", X"30", X"30", X"60", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00",    -- ï¿½
     X"00", X"00", X"00", X"18", X"30", X"60", X"60", X"60", X"60", X"60", X"30", X"18", X"00", X"00", X"00", X"00",    -- (
     X"00", X"00", X"00", X"18", X"0c", X"06", X"06", X"06", X"06", X"06", X"0c", X"18", X"00", X"00", X"00", X"00",    -- )
     X"00", X"00", X"00", X"00", X"00", X"6c", X"38", X"fe", X"38", X"6c", X"00", X"00", X"00", X"00", X"00", X"00",    -- *
@@ -249,8 +253,8 @@ architecture syn of vgaTextInterface is
     X"00", X"7e", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"7e", X"00",    -- empty
     X"00", X"7e", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"7e", X"00",    -- empty
     X"00", X"7e", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"7e", X"00",    -- empty
-    X"00", X"00", X"76", X"dc", X"00", X"00", X"dc", X"66", X"66", X"66", X"66", X"66", X"00", X"00", X"00", X"00",    -- ñ
-    X"00", X"00", X"76", X"dc", X"00", X"00", X"dc", X"66", X"66", X"66", X"66", X"66", X"00", X"00", X"00", X"00",    -- Ñ
+    X"00", X"00", X"76", X"dc", X"00", X"00", X"dc", X"66", X"66", X"66", X"66", X"66", X"00", X"00", X"00", X"00",    -- ï¿½
+    X"00", X"00", X"76", X"dc", X"00", X"00", X"dc", X"66", X"66", X"66", X"66", X"66", X"00", X"00", X"00", X"00",    -- ï¿½
     X"00", X"7e", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"7e", X"00",    -- empty
     X"00", X"7e", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"7e", X"00",    -- empty
     X"00", X"7e", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"42", X"7e", X"00",    -- empty
@@ -349,11 +353,11 @@ begin
     generic map ( FREQ_DIV => FREQ_DIV )
     port map ( clk => clk, line => line, pixel => pixel, R => color(11 downto 8), G => color(7 downto 4), B => color(3 downto 0), hSync => hSync, vSync => vSync, RGB => RGB );
   
-  colInt  <= ...;
-  uColInt <= ...;
+  colInt  <= pixel(9 downto 3);
+  uColInt <= pixel(2 downto 0);
   
-  rowInt  <= ...;
-  uRowInt <= ...;
+  rowInt  <= line(8 downto 4);
+  uRowInt <= line(3 downto 0);
   
   col  <= colInt;
   uCol <= uColInt;
@@ -361,55 +365,65 @@ begin
   row  <= rowInt;
   uRow <= uRowInt;
   
-------------------  
+  xy(11 downto 5) <= x;
+  xy(4 downto 0) <= y;
+  clearxy(11 downto 5) <= std_logic_vector (clearX);
+  clearxy(4 downto 0) <= std_logic_vector (clearY);
+  colrow(11 downto 5) <= colInt;
+  colrow(4 downto 0) <= rowInt;  
+  
+------------------ 
 
-  we        <= ...;
-  ramWrData <= ... when clearing='0' else ...;      
-  ramWrAddr <= ... when clearing='0' else ...; 
-  ramRdAddr <= ...;
+  we        <= dataRdy or clearing;
+  ramWrData <= char when clearing='0' else (others => '0');      
+  ramWrAddr <= xy when clearing='0' else clearxy; 
+  ramRdAddr <= colrow;
   
   process (clk)
   begin
     if rising_edge(clk) then
       if we='1' then
-        ram( ... ) <= ...;
+        ram(to_integer(unsigned(ramWrAddr))) <= ramWrData;
       end if; 
-      asciiCode <= ram( ... );
+      asciiCode <= ram(to_integer(unsigned(ramWrAddr)));
     end if;
   end process;
   
 ------------------  
   
-  romAddr <= ...;
- 
+  romAddr(11 downto 4) <= asciiCode;
+  romAddr(3 downto 0) <= uRowInt;
+  
   process (clk)
   begin
     if rising_edge(clk) then
-      bitMapLine <= rom( ... ) ;
+      bitMapLine <= rom(to_integer(unsigned(romAddr))) ;
     end if;
   end process;
 
-------------------  
+------------------  Revisar esta parte de aca abajo
 
   with uColInt select
     bitMapPixel <= 
-      ...
+      '0' when X"42",
+      '1' when others;
 
-  color <= ... when bitMapPixel='1' else ...;  
+  color <= (others => '0') when bitMapPixel='1' else (others => '1');  
   
 ------------------  
 
   clearCounters:
   process (clk, clearX, clearY, clear)
   begin
-    if ... then
+    if clear = '1' then
       clearing <= '1';
     else
       clearing <= '0';
     end if;
     if rising_edge(clk) then
       if clear='1' or clearing='1' then
-        ...
+        clearX <= (others => '0');
+        clearY <= (others => '0');
       end if;
     end if;
   end process; 
